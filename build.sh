fe6047f5b55463a44ca433b3e29c18fb2df28f8b
James Christopher Adduono <jc@adduono.com>
2016-01-09 18:46:23 -0500

    Add smarter build.sh and menuconfig.sh scripts for easy building

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# simple bash script for executing build
+
+RDIR=$(pwd)
+
+TOOLCHAIN=/home/jc/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
+
+THREADS=5
+
+[ "$VER" ] ||
+# version number
+VER=$(cat "$RDIR/VERSION")
+
+export ARCH=arm
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-cortex_a15-linux-gnueabihf-
+
+cd "$RDIR"
+
+[ "$DEVICE" ] || DEVICE=flo
+[ "$TARGET" ] || TARGET=nethunter
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] || {
+	echo "Config $DEFCONFIG not found in $ARCH configs!"
+	exit 1
+}
+
+export LOCALVERSION="$TARGET-$DEVICE-$VER"
+
+ABORT()
+{
+	echo "Error: $*"
+	exit 1
+}
+
+CLEAN_BUILD()
+{
+	echo "Cleaning build..."
+	cd "$RDIR"
+	rm -rf build
+}
+
+SETUP_BUILD()
+{
+	echo "Creating kernel config..."
+	cd "$RDIR"
+	mkdir -p build
+	make -C "$RDIR" O=build "$DEFCONFIG" || ABORT "Failed to set up build"
+}
+
+BUILD_KERNEL()
+{
+	echo "Starting build for $LOCALVERSION..."
+	while ! make -C "$RDIR" O=build -j"$THREADS"; do
+		read -p "Build failed. Retry? " do_retry
+		case $do_retry in
+			Y|y) continue ;;
+			*) return 1 ;;
+		esac
+	done
+}
+
+CLEAN_BUILD && SETUP_BUILD && BUILD_KERNEL && echo "Finished building $LOCALVERSION!"
