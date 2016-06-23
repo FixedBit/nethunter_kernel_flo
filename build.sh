7f3e921cfb0af688dc80ca933454a833ef00b993
James Christopher Adduono <jc@adduono.com>
2016-06-23 18:11:15 -0400

    Update build scripts, use newer toolchain

--- build.sh
+++ build.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
-# simple bash script for executing build
+# NetHunter kernel for Asus Google Nexus 7 (2013) build script by jcadduono
 
+################### BEFORE STARTING ################
+#
+# download a working toolchain and extract it somewhere and configure this
+# file to point to the toolchain's root directory.
+#
+# once you've set up the config section how you like it, you can simply run
+# DEVICE=[DEVICE] ./build.sh [VARIANT]
+#
+###################### CONFIG ######################
+
+# root directory of NetHunter flo git repo (default is this script's location)
 RDIR=$(pwd)
-
-TOOLCHAIN=/home/jc/build/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.4-2015.06
-
-THREADS=5
 
 [ "$VER" ] ||
 # version number
 VER=$(cat "$RDIR/VERSION")
 
+# directory containing cross-compile arm toolchain
+TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf
+
+# amount of cpu threads to use in kernel make process
+THREADS=5
+
+############## SCARY NO-TOUCHY STUFF ###############
+
 export ARCH=arm
-export CROSS_COMPILE=$TOOLCHAIN/bin/arm-cortex_a15-linux-gnueabihf-
+export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
 
-cd "$RDIR"
-
+[ "$1" ] && DEVICE=$1
 [ "$DEVICE" ] || DEVICE=flo
 [ "$TARGET" ] || TARGET=nethunter
+
 DEFCONFIG=${TARGET}_${DEVICE}_defconfig
-
-[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] || {
-	echo "Config $DEFCONFIG not found in $ARCH configs!"
-	exit 1
-}
-
-export LOCALVERSION="$TARGET-$DEVICE-$VER"
 
 ABORT()
 {
 	echo "Error: $*"
 	exit 1
 }
+
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] ||
+abort "Config $DEFCONFIG not found in $ARCH configs!"
+
+export LOCALVERSION=$TARGET-$DEVICE-$VER
+KDIR=$RDIR/build/arch/$ARCH/boot
 
 CLEAN_BUILD()
 {
@@ -42,10 +56,11 @@ CLEAN_BUILD()
 
 SETUP_BUILD()
 {
-	echo "Creating kernel config..."
+	echo "Creating kernel config for $LOCALVERSION..."
 	cd "$RDIR"
 	mkdir -p build
-	make -C "$RDIR" O=build "$DEFCONFIG" || ABORT "Failed to set up build"
+	make -C "$RDIR" O=build "$DEFCONFIG" \
+		|| ABORT "Failed to set up build"
 }
 
 BUILD_KERNEL()
